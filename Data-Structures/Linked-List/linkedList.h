4cb638a9ce6cb93d22493ecca119a76ef3a7dff8
Shashwat Yashaswi <Shashwat Yashaswi>
2016-10-03 02:30:51 +0530

    Fixing file structure. Also added destructor to handle out of scope

--- Data-Structures/Linked-List/linkedList.h
+++ Data-Structures/Linked-List/linkedList.h
@@ -14,9 +14,13 @@
 	*Value change of size might have been missed.
 	**********************************************************************************
 */
+
+#ifndef __LINKED_LIST__
+#define __LINKED_LIST__
+
 template <typename T>
 class linkedList{
-		struct node{		//Declares node which will keep the information of next node along with some value required by user
+		struct node{				//Declares node which will keep the information of next node along with some value required by user
 			T item; node *next;
 			node (T val, node* Next){
 				item = val;
@@ -24,34 +28,40 @@ class linkedList{
 			}
 		};
 		typedef node* link;
-		typedef link Node;		//link effectively points to node.
-		Node head;				//stores the address for first node
-		int size;				//stores the current size of the list
+		typedef link Node;			//link effectively points to node.
+		Node head, current;			//stores the address for first node
+		int size;					//stores the current size of the list
 	public:
-		linkedList();			//Constructor to initialize linked list
-		bool isEmpty() const;	//Checks and returns if list is empty
-		int size();				//Returns current size of the list
-		void addNode(T);		//Adds a node at the top
+		linkedList();				//Constructor to initialize linked list
+		~linkedList();				//Destructor to prevent memory leak if list goes out of scope
+		bool isEmpty() const;		//Checks and returns if list is empty
+		int size() const;			//Returns current size of the list
+		void push(T);				//Adds a node at the top
 		int insertAt(T, int);		//Inserts a new node at the i th position from the top
+		T pop ();					//Pops the last element and deletes the node
+		T delete_at(int);			//Deletes value at certain place
+		T returnValue();			//Returns value stored at current node 
 };
 
-linkedList::linkedList(){	//Initializes first node and size to 0
+linkedList::linkedList(){			//Initializes first node and size to 0
 	head = 0;
 	size = 0;
 }
 
-bool linkedList::isEmpty () const {
+bool linkedList::isEmpty () const{
 	head == 0;
 }
 
-int linkedList::size (){
+int linkedList::size () const{
 	return size;
 }
 
-int linkedList::addNode (T val){
+int linkedList::push (T val){
 	Node add (val, head);			//Creates a new node and links it to head
 	head = add;						//Head now points to the new node
 }
 
 void linkedList::insertAt (T x){
 }
+
+#endif
