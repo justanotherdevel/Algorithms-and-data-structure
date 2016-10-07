0c81cfa1f7698a4744c93f55b3d1a0166c4d280d
Shashwat Yashaswi <shashwatyashaswi@gmail.com>
2016-10-07 20:02:08 +0530

    Implemented destructor

--- Data-Structures/Linked-List/linkedList.h
+++ Data-Structures/Linked-List/linkedList.h
@@ -12,6 +12,7 @@
 /*
 	********************************Possible Errors***********************************
 	*Value change of size might have been missed.
+	*Check correctness of pop function
 	**********************************************************************************
 */
 
@@ -37,21 +38,29 @@ class linkedList{
 		bool isEmpty() const;		//Checks and returns if list is empty
 		int size() const;			//Returns current size of the list
 		void push(T);				//Adds a node at the top
-		int insertAt(T, int);		//Inserts a new node at the i th position from the top
+		bool insertAt(T, int);		//Inserts a new node at the i th position from the top
 		T pop ();					//Pops the last element and deletes the node
 		T delete_at(int);			//Deletes value at certain place
 		T returnValue();			//Returns value stored at current node 
+
 };
 
 template <typename T>
-linkedList<T>::linkedList(){			//Initializes first node and size to 0
+linkedList<T>::linkedList(){		//Initializes first node and size to 0
 	head = 0;
 	size = 0;
 }
 
 template <typename T>
+linkedlist<T>::~linkedlist(){
+	while (!isEmpty()){
+		pop();
+	}
+}
+
+template <typename T>
 bool linkedList<T>::isEmpty () const{
-	head == 0;
+	return head == 0;
 }
 
 template <typename T>
@@ -61,12 +70,23 @@ int linkedList<T>::size() const{
 
 template <typename T>
 int linkedList<T>::push (T val){
-	Node add (val, head);			//Creates a new node and links it to head
+	Node add = new node(val, head);	//Creates a new node and links it to head
 	head = add;						//Head now points to the new node
 }
 
 template <typename T>
-void linkedList<T>::insertAt (T x){
+bool linkedList<T>::insertAt (T x, int pos){
+	//Do something
+}
+
+template <typename T>
+T linkedList<T>::pop (){
+	Node temp = head;			//Creates a temporary node that points to head			
+	head = head->next;			//Head now points to the second node from top
+	T popValue = temp->item;	//Stores the value that we need to return
+	size--;						//Reduces current size of list by 1
+	delete temp;				//Deletes the temp node to regain memory
+	return popValue;
 }
 
 #endif
